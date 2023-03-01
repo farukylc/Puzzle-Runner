9a5080b1c7c3616f4880c424b6f97d66bdf5bc05
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-03-01 22:14:39 +0300

    update

--- Assets/Runner/Scripts/UIAnimations.cs
+++ Assets/Runner/Scripts/UIAnimations.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class UIAnimations : MonoBehaviour
 {
     [SerializeField] private Transform uıDoubleImage;
+    [SerializeField] private Image goldImage;
+    [SerializeField] private GameObject Canvas;
     private void Start()
     {
        InvokeRepeating("doPunchUI",1,0.5f);
@@ -16,5 +19,9 @@ public class UIAnimations : MonoBehaviour
     {
         uıDoubleImage.transform.DOPunchScale(Vector3.one * 0.25f, 0.49f, 1, 1f);
     }
-   
+
+    private void goldImageSpawner()
+    {   
+        Instantiate(goldImage.transform.gameObject, Canvas.transform);
+    }
 }
