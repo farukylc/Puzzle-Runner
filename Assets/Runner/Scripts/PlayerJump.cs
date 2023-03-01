55481c78c1bf92914f216a33c7360581ffeecd72
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-03-01 16:37:52 +0300

    Scripts

---
+++ Assets/Runner/Scripts/PlayerJump.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Animator anim;
+    public float jumpForce = 3;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "WallLine":
+
+                anim.SetBool("isJumping", true);
+                transform.DOJump(transform.position + new Vector3(0, 0, 15f), 3, 1, 1, false).SetEase(Ease.Linear);
+                break;
+            
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "WallLine":
+                anim.SetBool("isJumping",false);
+                break;
+        }   
+    }
+}
