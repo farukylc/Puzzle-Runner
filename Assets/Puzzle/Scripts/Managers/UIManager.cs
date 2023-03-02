3b29156e8cc63792a5507d7b4cb8d1621f196d9f
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2023-03-02 21:47:17 +0300

    PuzzleCat

--- Assets/Puzzle/Scripts/Managers/UIManager.cs
+++ Assets/Puzzle/Scripts/Managers/UIManager.cs
@@ -6,9 +6,11 @@ using DG.Tweening;
 public class UIManager : MonoBehaviour
 {
     GameManager _gameManager;
+    SceneControlManager _sceneManager;
     void Start()
     {
         _gameManager = GameManager.instance; 
+        _sceneManager = SceneControlManager.instance;
     }
 
     // Update is called once per frame
@@ -23,12 +25,20 @@ public class UIManager : MonoBehaviour
                 _gameManager.puzzlePieces[i].GetComponent<Renderer>().material = _gameManager.materials1[i];
             }
     }
-    public void OnClickSet2()
+    public void ButtonRun()
     {
         _gameManager.characterWithAnim.GetComponent<CharacterController>().CharacterRotate();
         // for (int i = 0; i < _gameManager.puzzlePieces.Length; i++)
         //     {
         //         _gameManager.puzzlePieces[i].GetComponent<Renderer>().material = _gameManager.materials2[i];
         //     }
+    }
+    public void ButtonNextPuzzle()
+    {
+        _sceneManager.NextPuzzleLevel();
+    }
+    public void ButtonPreviousPuzzle()
+    {
+        _sceneManager.PreviousPuzzleLevel();
     }
 }
