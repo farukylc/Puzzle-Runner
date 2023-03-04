f9426c00f5b95db0a9d160d32306ed52c8557a7d
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2023-03-05 00:25:33 +0300

    Puzzle

--- Assets/Runner/Scripts/TEST/Gate.cs
+++ Assets/Runner/Scripts/TEST/Gate.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gate : MonoBehaviour
 {
     [SerializeField] private GameObject givenObject;
     [SerializeField] private int legoNeedForObject;
     [SerializeField] private int legoDeposit;
-
+    [SerializeField] private Image gateImage;
+    [SerializeField] private Image gateImageBackground;
 
     [SerializeField] private GameObject gateWP;
 
@@ -25,23 +27,24 @@ public class Gate : MonoBehaviour
                     
                 }
                 
-                break;
-            
+            break;
             
             case  "CollectableLego":
-
                 legoDeposit++;
-                PlayerCollect.playerCollectScript.collectedItems[^1].transform.position = gateWP.transform.position;
-                PlayerCollect.playerCollectScript.collectedItems[^1].gameObject.transform.SetParent(transform);
+                if(legoDeposit >= legoNeedForObject)
+                    break;
+                gateImage.fillAmount += legoDeposit/(float)legoNeedForObject;
+                gateImageBackground.fillAmount -= legoDeposit/(float)legoNeedForObject;
+                GameObject currentObj = PlayerCollect.playerCollectScript.collectedItems[PlayerCollect.playerCollectScript.collectedItems.Count-1];
+                currentObj.GetComponent<SmoothDamp>().enabled = false;
+                currentObj.transform.position = gateWP.transform.position;
+                currentObj.transform.SetParent(transform);
                 gateWP.transform.position += new Vector3(0, 0.5f, 0);
-                 PlayerCollect.playerCollectScript.collectedItems.Remove(
+                PlayerCollect.playerCollectScript.collectedItems.Remove(
                    PlayerCollect.playerCollectScript.collectedItems[^1]);
-                
                 PlayerCollect.playerCollectScript.waypoint.transform.position -= new Vector3(0, 0f, 1f);
-
-                other.gameObject.GetComponent<SmoothDamp>().enabled = false;
                 
-                break;
+            break;
         }
     }
 }
