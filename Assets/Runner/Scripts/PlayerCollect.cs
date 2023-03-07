21bad24e15f3c623541e88b24c8c82cd094dd4a5
Faruk Yolcu <ce.farukyolcu@gmail.com>
2023-03-07 16:13:41 +0300

    player collect script updated

--- Assets/Runner/Scripts/PlayerCollect.cs
+++ Assets/Runner/Scripts/PlayerCollect.cs
@@ -10,72 +10,28 @@ public class PlayerCollect : MonoBehaviour
 {
     public static PlayerCollect playerCollectScript;
     [SerializeField] public int collectedLegos = 0;
-    //[SerializeField] private GameObject legoIcon;
     private bool isPunch = false;
     private bool goldPunch = false;
-    // [SerializeField] public int bonusScore = 0;
     [SerializeField] public bool isObjectOpen = false;
      
     //UI
-    //[SerializeField] private GameObject smoke;
     [SerializeField] public int goldAmount = 0;
-    //[SerializeField] public Slider scoreSlider;
-    //[SerializeField] private TextMeshProUGUI goldAmountText;
-    
-   
-    //[SerializeField] private int maxScore;
-    [SerializeField] public int targetScore;
     [SerializeField] private Image goldIcon;
-    //[SerializeField] private Image qmImage;
-    //[SerializeField] private Sprite winObject;
+    [SerializeField] private TextMeshProUGUI goldAmountText;
     
-    
-    
-    //Stack
-    //[SerializeField] public GameObject waypoint;
+    //Lists
     [SerializeField] public List<GameObject> collectedItems = new List<GameObject>();
-    
-    
     private void Start()
     {
-        //scoreSlider.maxValue = targetScore;
-       
         playerCollectScript = this;
         InvokeRepeating("qmAnimation",1,1);
     }
     
-    private void Update()
-    {
-        if (targetScore <= collectedLegos)
-        {
-            CancelInvoke("qmAnimation");
-            //qmImage.sprite = winObject;
-        }
-           
-    }
-
-    private void qmAnimation()
-    {
-        // if(collectedLegos < targetScore)
-        //     //qmImage.transform.DOPunchScale(Vector3.one * 0.5f, 0.5f, 1, 1f);
-    }
-
-    // private void stackFunction(Collider other)
-    // {
-    //     other.transform.DOLocalJump(waypoint.transform.localPosition, 1.5f, 1, 0.2f);
-    //     other.transform.SetParent(transform.gameObject.transform);
-    //     other.transform.localScale = other.transform.localScale / 1.25f;
-    //     other.transform.localRotation = Quaternion.Euler(0,90,0);
-    //     other.GetComponent<LegoAnimation>().enabled = false;
-    //     other.GetComponent<BoxCollider>().enabled = false;
-    //     waypoint.transform.position += new Vector3(0, 0f, 1f); //y 0.44f
-    //     collectedItems.Add(other.gameObject);
-    // }
+    
     public void Collect(GameObject obj)
     {
         collectedLegos = collectedLegos + 1;
-        //scoreSlider.value = collectedLegos;
-        
+       
         if(!collectedItems.Contains(obj))
         {
            
@@ -104,7 +60,6 @@ public class PlayerCollect : MonoBehaviour
         }
         else
         {
-            //objectCount--;
             GameObject lastObject = collectedItems[collectedItems.Count-1];
             lastObject.SetActive(false);
             lastObject.transform.parent = null;
@@ -131,29 +86,14 @@ public class PlayerCollect : MonoBehaviour
             case "CollectableLego":
                 
                 Collect(other.gameObject);
-               
-                // stackFunction(other);
-                if (collectedLegos == targetScore)
-                {
-                    isObjectOpen = true;
-                }
-
-                if (!isPunch && collectedLegos < targetScore)
-                {
-                    isPunch = true;
-                    // legoIcon.transform.DOPunchScale(Vector3.one * 0.5f, 0.5f, 1, 1f).OnComplete((() => 
-                    //         isPunch = false
-                    //     ));
-
-                }
-               
+                
                 break;
-            
             
             case "Gold":
                 goldAmount++;
-                // goldAmountText.text = goldAmount.ToString();
                 Destroy(other.gameObject);
+
+                goldAmountText.text = goldAmount.ToString();
                 if (!goldPunch)
                 {
                     goldPunch = true;
@@ -165,8 +105,6 @@ public class PlayerCollect : MonoBehaviour
                 break;
         }
 
-        
-       
     }
     
     
