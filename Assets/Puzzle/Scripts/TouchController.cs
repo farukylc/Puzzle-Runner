40bf325829addde101739182f31c6e59be829bca
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2023-03-09 02:27:18 +0300

    Puzzle

--- Assets/Puzzle/Scripts/TouchController.cs
+++ Assets/Puzzle/Scripts/TouchController.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 
 public class TouchController : MonoBehaviour
 {
+    public static TouchController instance;
     public GameObject tapToBuild,
                       firstPuzzlePiece,
                       firstPuzzleContent;
     public ScrollRect scroll;
     public bool isStart;
+    public void Awake() 
+    {
+        instance = this;
+    }
     void Start()
     {
         
@@ -25,8 +30,14 @@ public class TouchController : MonoBehaviour
             tapToBuild.SetActive(false);
             gameObject.SetActive(false);
             scroll.DOHorizontalNormalizedPos(1,3f).OnComplete(()=>
-                scroll.DOHorizontalNormalizedPos((float)firstPuzzleContent.transform.GetSiblingIndex()/scroll.content.transform.childCount,1f).OnComplete(()=>
+                scroll.DOHorizontalNormalizedPos((float)(firstPuzzleContent.transform.GetSiblingIndex()+1)/scroll.content.transform.childCount,1f).OnComplete(()=>
                     firstPuzzlePiece.GetComponent<Animator>().enabled = true));
         }
+    }
+    public void ScrollMove(GameObject target)
+    {
+        Debug.Log((float)(target.transform.parent.transform.parent.transform.GetSiblingIndex()+1)/scroll.content.transform.childCount);
+        scroll.DOHorizontalNormalizedPos((float)(target.transform.parent.transform.parent.transform.GetSiblingIndex()+1)/scroll.content.transform.childCount,1f).OnComplete(()=>
+                    target.GetComponent<Animator>().enabled = true);
     }
 }
