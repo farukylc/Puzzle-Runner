f5eb11bd1f546d03712bbfe5a4aed40db007bfb0
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2023-03-11 16:57:14 +0300

    Puzzle

--- Assets/Puzzle/Scripts/JsonController.cs
+++ Assets/Puzzle/Scripts/JsonController.cs
@@ -7,34 +7,53 @@ public class JsonController : MonoBehaviour
 {
     public UserStats user1 = new UserStats();
     
-    void Start()
-    {
-        if(!File.Exists(Application.dataPath + "/Saves/UserData.json"))
-        {
-            JsonSave();
-        }
-        else if(File.Exists(Application.dataPath + "/Saves/UserData.json"))
-        {
-            JsonLoad();
-        }
-    }
+    // void Start()
+    // {
+    //     if(!File.Exists(Application.dataPath + "/Saves/UserData.json"))
+    //     {
+    //         JsonSave();
+    //     }
+    //     else if(File.Exists(Application.dataPath + "/Saves/UserData.json"))
+    //     {
+    //         JsonLoad();
+    //     }
+    // }
+    // public void JsonSave()
+    // {
+    //     string jsonString = JsonUtility.ToJson(user1);
+    //     File.WriteAllText(Application.dataPath + "/Saves/UserData.json", jsonString);
+    // }
+    // public void JsonLoad()
+    // {
+    //     string dataPath = Application.dataPath + "/Saves/UserData.json";
+    //     if(File.Exists(dataPath))
+    //     {
+    //         string jsonString = File.ReadAllText(dataPath);
+    //         user1 = JsonUtility.FromJson<UserStats>(jsonString); 
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Kayit bulunamadi");
+    //     }
+    // }
     public void JsonSave()
     {
-        string jsonString = JsonUtility.ToJson(user1);
-        File.WriteAllText(Application.dataPath + "/Saves/UserData.json", jsonString);
+        var json = JsonUtility.ToJson(user1);
+        File.WriteAllText(Application.persistentDataPath + Path.DirectorySeparatorChar + "GameData.txt", json);
     }
+
     public void JsonLoad()
     {
-        string dataPath = Application.dataPath + "/Saves/UserData.json";
-        if(File.Exists(dataPath))
+        if (File.Exists(Application.persistentDataPath + Path.DirectorySeparatorChar + "GameData.txt"))
         {
-            string jsonString = File.ReadAllText(dataPath);
-            user1 = JsonUtility.FromJson<UserStats>(jsonString); 
+            var json = File.ReadAllText(Application.persistentDataPath + Path.DirectorySeparatorChar + "GameData.txt");
+
+            JsonUtility.FromJsonOverwrite(json, user1);
         }
         else
         {
-            Debug.Log("Kayit bulunamadi");
+            var json = JsonUtility.ToJson(user1);
+            File.WriteAllText(Application.persistentDataPath + Path.DirectorySeparatorChar + "GameData.txt", json);
         }
     }
-
 }
