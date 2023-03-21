83c5d8b8b9badcf3cc870871c401f1ca78f17a64
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2023-03-21 18:27:55 +0300

    Puzzle

---
+++ Assets/Puzzle/Scripts/FirstLevelController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstLevelController : MonoBehaviour
+{
+    void Start()
+    {
+        if(GameManager.instance.currentLevel == 1)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Puzzle/Scripts/Managers/SceneControlManager.cs
+++ Assets/Puzzle/Scripts/Managers/SceneControlManager.cs
@@ -14,7 +14,6 @@ public class SceneControlManager : MonoBehaviour
     {
         
     }
-
     void Update()
     {
         
